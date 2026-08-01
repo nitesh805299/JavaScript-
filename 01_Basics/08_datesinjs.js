041a61b26100805ea918eacd1a4ef360f6043759
nitesh805299 <niteshchaurasia515@gmail.com>
2026-08-01 11:52:10 +0530

    complete Basic part 1

---
+++ 01_Basics/08_datesinjs.js
@@ -0,0 +1,30 @@
+// dates 
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+// console.log(typeof(myDate));
+
+// let myCreatedDate = new Date (2026,8,23);
+ let myCreatedDate = new Date (2026,8,23,5,30);
+// let myCreatedDate = new Date ("2026-08-01");
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);// date nad tie in milisecond 
+// console.log(myCreatedDate.getTime());//date and time in milisecond
+
+// console.log(Math.floor((Date.now())/1000));// it is use to find the difference between two dates in days
+ let newDate = new Date();
+//  console.log(newDate);
+//  console.log(newDate.getFullYear());
+//  console.log(newDate.getMonth());
+//  console.log(newDate.getMonth()+1);
+//  console.log(newDate.getDay());
+ 
+
+//  console.log(newDate.toLocaleString('defult', {weekday:'long', year:'numeric', month:'long', day:'numeric'}));
