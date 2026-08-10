86f515106deaebb50dae2b558844a86000ddbcbe
nitesh805299 <niteshchaurasia515@gmail.com>
2026-08-10 23:22:51 +0530

    advance_one

---
+++ 07_Project/05_project/script.js
@@ -0,0 +1,1 @@
+const insert = document.querySelector("#insert");
---
+++ 07_Project/06_project/script.js
@@ -0,0 +1,31 @@
+// Generate a random color
+
+const randomColor = function () {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+};
+
+let intervalId = null;
+
+document.querySelector("#start").addEventListener("click", function () {
+
+    if (intervalId === null) {
+        intervalId = setInterval(function () {
+            document.body.style.backgroundColor = randomColor();
+        }, 1000);
+    }
+
+});
+
+document.querySelector("#stop").addEventListener("click", function () {
+
+    clearInterval(intervalId);
+    intervalId = null;
+
+});
